be503183cb827565523aae51a1bd39cc75be251c
Sauron <git@saur0n.science>
2023-10-03 19:17:13 +0300

    Moved FileWriter to a separate header file

--- FileWriter.cpp
+++ FileWriter.cpp
@@ -4,19 +4,14 @@
  *  © 2016—2023, Sauron
  ******************************************************************************/
 
-#include "FileSerialization.hpp"
+#include "FileWriter.hpp"
 
 using namespace rohan;
 
 /******************************************************************************/
 
-FileSerializer::FileSerializer(const char * filename, int flags) :
-        file(filename, flags) {}
-
-/******************************************************************************/
-
 FileWriter::FileWriter(const char * filename, int flags) :
-        FileSerializer(filename, flags) {}
+        file(filename, flags) {}
 
 void FileWriter::write(const void * from, size_t length) {
     file.write(from, length);
--- FileWriter.hpp
+++ FileWriter.hpp
@@ -4,28 +4,26 @@
  *  © 2016—2023, Sauron
  ******************************************************************************/
 
-#ifndef __ROHAN_FILESERIALIZATION_HPP
-#define __ROHAN_FILESERIALIZATION_HPP
+#ifndef __ROHAN_FILEWRITER_HPP
+#define __ROHAN_FILEWRITER_HPP
 
 #include <unix++/File.hpp>
 #include "Serialization.hpp"
 
 namespace rohan {
 
-/** Base class for FileReader and FileWriter **/
-class FileSerializer {
-protected:
-    FileSerializer(const char * filename, int flags);
-    upp::File file;
-};
-
 /**/
-class FileWriter : public Writer, FileSerializer {
+class FileWriter : public Writer {
 public:
     /** Open a file for writing **/
     explicit FileWriter(const char * filename, int flags=O_WRONLY|O_CREAT|O_TRUNC);
     /** Write a portion of data **/
     void write(const void * from, size_t length) override;
+    /** Direct access to the underlying file **/
+    upp::File &getFile() { return file; }
+    
+private:
+    upp::File file;
 };
 
 }
